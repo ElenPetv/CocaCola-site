bd56a406e80af7fb9dce06f8d69ec37ca09649b9
ElenPetv <elen.ptr02@gmail.com>
2022-02-01 00:56:19 +0200

    JS add smooth scroll for menu items

--- app.js
+++ app.js
@@ -5,22 +5,38 @@ var header = $("header");
 var intro = $("intro");
 
 var currentHeight = $('#intro').innerHeight();
-    console.log(currentHeight);
 
 var scrollPos = $(window).scrollTop();
-    console.log(scrollPos);
 
+checkScroll(scrollPos , currentHeight);
 
-$(window).on("scroll load resize", function(){
-    currentHeight = $('#intro').innerHeight();
-    scrollPos = $(this).scrollTop();
-    if( scrollPos > currentHeight){
-        header.addClass("fixed");
-    }else{
-        header.removeClass("fixed");
+    $(window).on("scroll resize", function(){
+        currentHeight = $('#intro').innerHeight();
+        scrollPos = $(this).scrollTop();
+
+        checkScroll(scrollPos , currentHeight);
+    });
+
+    function checkScroll(scrollPos , currentHeight){
+        if( scrollPos > currentHeight){
+            header.addClass("fixed");
+        }else{
+            header.removeClass("fixed");
+        }
     }
+
+
+/*SMOOTH SCROLL*/
+$("[data-scroll]").on("click", function(Event){
+    Event.preventDefault();
+
+    var elementId = $(this).data('scroll');
+    var elementOffset = $(elementId).offset().top;
+
+    $("html, body").animate({
+        scrollTop:elementOffset - 50
+    }, 700);
+
 });
-
-
 
 });
